e80d47152fb2fc2f7d9afc5785461cb7e94e5e08
AmandineGUISY <aguisy@yahoo.fr>
2025-07-04 00:14:22 +0200

    REFAC : Enhance image gallery functionality with API integration and improved layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <script setup lang="ts">
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
 
+interface Photo {
+  id: number;
+  title: string;
+  category: string;
+  tags: string[];
+  image_url: string;
+  thumbnail_url: string;
+}
 
+const API_BASE_URL = 'http://127.0.0.1:8000';
 
+const images = ref<Photo[]>([]);
 
-import HelloWorld from './components/HelloWorld.vue'
+onMounted(async () => {
+    getImages();
+})
+
+const getImages = async () => {
+  try {
+      const response = await axios.get(`${API_BASE_URL}/photos`);
+      images.value = response.data;
+      console.log(images.value);
+
+  } catch (error){
+    console.error("Une erreur est survenue lors de la récupération des images", error);
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <div class="btn btn-primary"> LOL </div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1>Mini Gallerie</h1>
+  <div v-if="images.length > 0" class="gallery">
+    <div v-for="image in images" :key="image.id" class="photo-card">
+      <img :src="`${API_BASE_URL}${image.thumbnail_url}`" :alt="image.title" />
+      <h2>{{ image.title }}</h2>
+      <p>{{ image.category }}</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div v-else>
+    <p>Chargement des images...</p>
+  </div>
 </template>
 
 <style scoped>
+.gallery {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+.photo-card {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 1rem;
+  text-align: center;
+}
 </style>
