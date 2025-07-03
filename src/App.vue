595a4b318330bd5b19bc2bb883b03012a31b716b
AmandineGUISY <aguisy@yahoo.fr>
2025-07-03 18:29:49 +0200

    ADD : installation of bootstrap and axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+
+
+
+
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
+    <div class="btn btn-primary"> LOL </div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -15,16 +20,4 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
