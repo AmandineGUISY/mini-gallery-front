6885af324f9b60c48c97bdf1a668a032876e4f1a
AmandineGUISY <aguisy@yahoo.fr>
2025-07-04 23:18:16 +0200

    REFAC : Simplify image handling by removing redundant Photo interface and enhancing template structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
-
-interface Photo {
-  id: number;
-  title: string;
-  category: string;
-  tags: string[];
-  image_url: string;
-  thumbnail_url: string;
-}
+import { type Photo } from './types/index.tsx';
 
 const API_BASE_URL = 'http://127.0.0.1:8000';
 
@@ -34,11 +26,13 @@ const getImages = async () => {
 
 <template>
   <h1>Mini Gallerie</h1>
-  <div v-if="images.length > 0" class="gallery">
-    <div v-for="image in images" :key="image.id" class="photo-card">
-      <img :src="`${API_BASE_URL}${image.thumbnail_url}`" :alt="image.title" />
-      <h2>{{ image.title }}</h2>
-      <p>{{ image.category }}</p>
+  <div v-if="images.length > 0" class="card">
+    <div v-for="image in images" :key="image.id" >
+      <h2 class="card-header">{{ image.title }}</h2>
+      <div class="card-body">
+        <img :src="`${API_BASE_URL}${image.thumbnail_url}`" :alt="image.title" />
+        <p>{{ image.category }}</p>
+      </div>
     </div>
   </div>
   <div v-else>
@@ -46,16 +40,3 @@ const getImages = async () => {
   </div>
 </template>
 
-<style scoped>
-.gallery {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-}
-.photo-card {
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 1rem;
-  text-align: center;
-}
-</style>
---
+++ src/types/index.tsx
@@ -0,0 +1,8 @@
+export interface Photo {
+  id: number;
+  title: string;
+  category: string;
+  tags: string[];
+  image_url: string;
+  thumbnail_url: string;
+}
