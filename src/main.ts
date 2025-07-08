9b3137c5334e54ed801bc3a9f58c067355b360a4
AmandineGUISY <aguisy@yahoo.fr>
2025-07-08 23:27:59 +0200

    FEAT : Integrate vue-toastification for enhanced user notifications

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,21 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlus, faPenToSquare, faXmark, faImage, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+const app = createApp(App)
 
 library.add( faPlus, faPenToSquare, faXmark, faImage, faTrash, faCheck )
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+app.use(Toast, {
+  timeout: 3000,
+  position: 'top-right',
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+})
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
